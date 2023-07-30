7d212b238fa2d0e52fd27a5e8fb75d5e10ca7cb8
Kirti Kamal <98299441+KirtiKamal@users.noreply.github.com>
2023-07-30 06:20:56 +0000

    final

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Kirti Kamal Sahoo",
+  title: "Web Designer & Devops",
+  email: "kirtikamal.sahoo@studentambassador.com",
+  gitHub: "KirtiKamal",
+  instagram: "kirtikml",
+  linkedIn: "kirtikamal",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "kirtikamal12",
+ 
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "My Tech Stack";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,11 +32,11 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Dev Ops",
+  "Open Source",
+  "Java",
+  "JavaScript",
+  "React",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Pursuing my bachelor’s degree in Computer Science and Engineering. I am a highly competent software professional with a proven track record in designing websites, networking, and open source contributor. I have strong technical skills as well as excellent interpersonal skills, enabling me to interact with new people. I am eager to be challenged in order to grow and further improve my software skills. My main goal is to demystify technology for everyone.";
 
 const About = () => {
   return (
